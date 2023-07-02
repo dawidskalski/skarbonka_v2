7fa1f611af8f5aae58276387c32e41dc4baa730f
Dawid Skalski <dawidskalski10@gmail.com>
2023-07-02 12:01:20 +0200

    add dispose notifyHelper

--- lib/app/features/home/home_page.dart
+++ lib/app/features/home/home_page.dart
@@ -39,6 +39,13 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    notifyHelper.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
